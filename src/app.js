b775e533021ccdefd5492a872c99955fe205f585
Carlos Perren <idux.net@gmail.com>
2023-08-10 11:29:30 -0300

    Aclaración cors

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express()
 // Habilitamos el uso de cors (https://reflectoring.io/complete-guide-to-cors/)
 app.use(express.json());
 app.use(cors({
-    origin: '*'
+    origin: '*' // Se aceptan solicitudes desde cualquier origen
 }));
 app.use(express.urlencoded({ extended: true }));
 
@@ -19,13 +19,14 @@ app.use(express.urlencoded({ extended: true }));
 // En lugar de declarar los endpoints acá, lo hacemos en un archivo de rutas por separado
 app.use('/api/giftcards', giftcardsRoutes());
 
+
 // Habilitamos una ruta "catchall" para retornar un contenido amigable cuando se intenta
 // acceder a un endpoint que no existe
 app.all('*', (req, res) => {
-    res.status(200).send({ status: 'OK', data: 'No se encuentra el endpoint solicitado' })
+    res.status(404).send({ status: 'OK', data: 'No se encuentra el endpoint solicitado' })
 })
 
 // Finalmente ponemos a "escuchar" nuestro servidor en un puerto específico
 app.listen(EXPRESS_PORT, () => {
-    console.log(`Backend iniciado en puerto ${EXPRESS_PORT}`)
+    console.log(`Backend inicializado puerto ${EXPRESS_PORT}`)
 })
