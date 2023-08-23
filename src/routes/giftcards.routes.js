8ab7becf4c13cf0251b2b56ec541c2c63669ced4
Carlos Perren <idux.net@gmail.com>
2023-08-23 12:26:17 -0300

    Conexión a bbdd Mongo Atlas

--- src/routes/giftcards.routes.js
+++ src/routes/giftcards.routes.js
@@ -1,13 +1,19 @@
 import { Router } from 'express'
-import giftcards_data from '../helpers/giftcards.js'
+// Ya no necesitamos el array de giftcards local, recuperamos desde la base de datos
+// import giftcards_data from '../helpers/giftcards.js'
+// Importamos el modelo para poder invocar a través de él los distintos métodos de consulta
+import giftcardModel from '../models/giftcard.model.js'
 
 // Es muy común utilizar distintos archivos de rutas para organizar los endpoints,
 // luego agregaremos el uso de clases.
 export const giftcardsRoutes = ()  => {
     const router = Router()
 
-    router.get('/', (req, res) => {
-        res.status(200).send({ status: 'OK', data: giftcards_data })
+    // ES MUY IMPORTANTE recordar siempre el uso de promesas, sea con then catch o async await
+    // al consultar bases de datos o manejar archivos
+    router.get('/', async (req, res) => {
+        const giftcards = await giftcardModel.find()
+        res.status(200).send({ status: 'OK', data: giftcards })
     })
 
     return router
