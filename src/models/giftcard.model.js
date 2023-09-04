874b588903e0ff4295edd52356a75574f2670640
Carlos Perren <idux.net@gmail.com>
2023-09-04 11:42:11 -0300

    Cambio price a string

--- src/models/giftcard.model.js
+++ src/models/giftcard.model.js
@@ -6,7 +6,7 @@ const collection = 'giftcards'
 
 const schema = new mongoose.Schema({
     title: { type: String, required: true },
-    price: { type: Number, required: true },
+    price: { type: String, required: true },
     image: { type: String }
 });
 
